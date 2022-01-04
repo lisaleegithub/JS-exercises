64d3f2f9ce2e0b4e1a7dfbd77bd190d2d38d617e
Lisa Lee <lisawlee04@gmail.com>
2022-01-03 22:35:10 -0800

    Complete prework 5.

--- prework/5_ArraysObjectsExercise.js
+++ prework/5_ArraysObjectsExercise.js
@@ -10,28 +10,40 @@
 // But make sure you understand why the correct answer is right.
 
 // Exercise 1. Make an empty array named animals
+const animals = [];
 
 // Exercise 2. Add the string "frog" to the array
+animals.push("frog");
 
 // Exercise 3. Add 4 more animals of your choice to the array. Try to add all of them in one line of code.
+animals.push("penguin", "sloth", "deer", "otter");
 
 // Exercise 4. Update the first item in the array to be "gorilla"
+animals[0] = "gorilla";
 
 // Exercise 5. Print the number of items in the array (use the array property that will give you this information)
+console.log(animals.length);
 
 // Exercise 6. Print the first item in the array
+console.log(animals[0]);
 
 // Exercise 7. Print the last item in the array (as part of your answer, use the same array property you used in exercise 5 above)
+console.log(animals[animals.length-1]);
 
 // Exercise 8. Remove the last item from the array
+animals.pop(-1);
 
 // Exercise 9. Now make a new array named assortedThings that contains at least 3 strings and 3 numbers.
+const assortedThings = ["coffee", "fish oil", "phone", 5, 50, 100];
 
 // Exercise 10. Write a for loop that iterates through the assortedThings array and prints each index and item, such as:
 // Item #0 is potato
 // Item #1 is 4
 // Item #2 is Hello World
 // ...
+for (var thing = 0; thing < assortedThings.length; thing++) {
+    console.log ("Item #" + thing + " is " + assortedThings[thing]);
+  } 
 
 // Exercise 11. Write a function that takes an array of numbers as a parameter. For each number in the array,
 // print the number and "BIG" if it's over 100, "small" if it's between 0 and 100, and "negative" if
@@ -42,21 +54,44 @@
 // 50 small
 //
 // Then write some function calls you would use to test your function.
+function compareNum(numbers) {
+    for(let numIndex = 0; numIndex < numbers.length; numIndex++) {
+        if(numbers[numIndex] > 100) {
+        console.log(numbers[numIndex] + " BIG");
+      } else if(numbers[numIndex] >= 0 && numbers[numIndex] <= 100) {
+        console.log(numbers[numIndex] + " small");
+      } else {
+        console.log(numbers[numIndex] + " negative");
+      }
+    }
+  }
+  compareNum([-2, 200, 50]);
+  compareNum([-2, 0, 50, 100, 101, 200]);
 
 // Exercise 12. Make an object called me that contains the following keys: name, favoriteAnimal, favoriteNumber
 // The values of those keys can be whatever you choose.
+const me = {
+  name : "Lisa",
+  favoriteAnimal : "penguin",
+  favoriteNumber : 2
+}
 
 // Exercise 13. Add another 2 keys and values to the object (you choose what they are)
+me.favoriteColor = "lavender";
+me.favoriteSoda = "Diet Coke";
 
 // Exercise 14. Update the favoriteAnimal value to something different
+me.favoriteAnimal = "beluga whale";
 
 // Exercise 15. Print the value of favoriteAnimal.
 // Note: there are at least 2 ways to get the value of a key, try to write both.
+console.log(me.favoriteAnimal);
+console.log(me["favoriteAnimal"]);
 
 // Congrats, you made it to the end!
 // Did you find this easy or hard? If you used references, which ones helped you?
 // Please answer in a comment below.
-//
+// This assignment was easy because it was bascially creating and updating arrays and objects.
 
 // Email your file to the course staff,
 // or commit your file to GitHub and email us a link.
